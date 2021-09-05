12e1cdf19c07b0fd84c0f706920a63a7a3d99215
Michael VanBemmel <michael.vanbemmel@gmail.com>
2021-09-04 20:55:53 -0700

    Build rust std to make the wasm smaller.

--- ignition-hello-world/build.sh
+++ ignition-hello-world/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
+cargo_flags="-Z build-std=core,alloc --target wasm32-unknown-unknown"
 build_dir=../target/wasm32-unknown-unknown/release
-cargo +nightly build --target wasm32-unknown-unknown
-cargo +nightly build --target wasm32-unknown-unknown --release
+cargo +nightly build $cargo_flags
+cargo +nightly build $cargo_flags --release
 wasm-opt -o $build_dir/opt.wasm -Oz $build_dir/ignition_hello_world.wasm
 ls -lh $build_dir/opt.wasm
